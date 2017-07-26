8aed24c40f3a4b4ad945de0e66eaf04ffc051495
Marco Rosa <marco1rosa@gmail.com>
2017-07-26 13:30:52 +0200

    JS

---
+++ UTE SANVITO/ute-templates-static/static/js/ute.js
@@ -0,0 +1,57 @@
+
+
+
+window.onscroll = function(){
+    var menu = document.getElementById("menu");
+    var spacer = document.getElementById("menu-spacer");
+    var logo = document.getElementsByClassName("navbar-brand")[0].children;
+    
+    if(document.body.scrollTop >= 180 && document.body.clientWidth >= 768){
+        if(!menu.classList.contains("fixed-menu")){
+            addClass(menu, "fixed-menu");
+        }
+            addClass(spacer, "spacer");
+            show(logo[0], true);
+            show(logo[1], true);
+        
+    } else if(document.body.scrollTop < 180){
+        if(menu.classList.contains("fixed-menu")){
+            removeClass(menu, "fixed-menu");
+        }
+        removeClass(spacer, "spacer");
+        show(logo[0], false);
+        show(logo[1], false);
+        
+    } else if(document.body.scrollTop >= 0 && document.body.clientWidth < 768){
+        if(!menu.classList.contains("fixed-menu")){
+            addClass(menu, "fixed-menu");
+        }
+        show(logo[0], false);
+        show(logo[1], false);
+    }
+}
+
+
+
+function show(elem, toShow){
+    if(toShow){
+        elem.classList.remove("hide");
+        elem.classList.add("show");
+    } else {
+        elem.classList.add("hide");
+        elem.classList.remove("show");
+    }
+}
+
+
+
+function addClass(elem, theClass){
+    elem.classList.add(theClass);
+}
+
+function removeClass(elem, theClass){
+    elem.classList.remove(theClass);
+}
+
+
+
